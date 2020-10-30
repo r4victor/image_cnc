982906c3b2f14149c081092349543c595180a7c9
Victor Skvortsov <vds003@gmail.com>
2020-10-30 15:36:01 +0500

    Add PSNR caclulation and conversion to grayscale

--- image_cnc/core.py
+++ image_cnc/core.py
@@ -1,18 +1,133 @@
+from dataclasses import dataclass
 import io
+import math
 
+import numpy as np
 import PIL.Image
 
 
-def upload_image(filepath):
+@dataclass
+class ImageState:
+    visible_image: PIL.Image.Image
+    real_image: PIL.Image.Image
+
+
+def upload_image(filepath: str) -> ImageState:
     image = PIL.Image.open(filepath)
-    return image
+    image_state = ImageState(visible_image=image, real_image=image)
+    return image_state
 
 
-def image_to_bytes(image):
+def image_filename(image_state: ImageState) -> str:
+    return image_state.real_image.filename
+
+
+def image_to_bytes(image_state: ImageState):
+    return _image_to_bytes(image_state.visible_image)
+
+
+def _image_to_bytes(image: PIL.Image.Image) -> bytes:
     bio = io.BytesIO()
     image.save(bio, format='PNG')
     return bio.getvalue()
 
 
-def save_image(filepath, image):
+def save_image(filepath: str, image_state: ImageState) -> None:
+    image = image_state.real_image
     image.save(filepath, 'PNG')
+
+
+MODE_DEPTHS = {
+    'L': 8,
+    'RGB': 8,
+}
+PSNR_SUPPORTED_MODES = set(MODE_DEPTHS.keys())
+
+
+def psnr(image_state1: ImageState, image_state2: ImageState) -> float:
+    return _psnr(image_state1.real_image, image_state2.real_image)
+
+
+def _psnr(image1: PIL.Image.Image, image2: PIL.Image.Image) -> float:
+    for image in (image1, image2):
+        if image.mode not in {'L', 'RGB'}:
+            raise ValueError(
+                'Cannot calculate PSNR for {image.mode} image mode. '
+                'Supported image modes are: {PSNR_SUPPORTED_MODES}.'
+            )
+
+    if image1.mode != image2.mode:
+        raise ValueError(
+            'Cannot calculate PSNR for images of different modes. '
+            f'Left image has mode {image1.mode}, but right image has mode {image2.mode}.'
+        )
+    if image1.size != image2.size:
+        raise ValueError(
+            'Cannot calculate PSNR for images of different sizes. '
+            f'Left image has size {image1.size}, but right image has size {image2.size}.'
+        )
+
+    a1 = np.asarray(image1)
+    a2 = np.asarray(image2)
+
+    if len(a1.shape) == 2:
+        height, width = a1.shape
+        n_channels = 1
+    else:
+        height, width, n_channels = a1.shape
+    N = height * width * n_channels
+    depth = MODE_DEPTHS[image.mode]
+
+    square_error_sum = ((a1 - a2) ** 2).sum()
+    if square_error_sum == 0:
+        return float('inf')
+
+    max_square_error_sum = N * (2 ** depth - 1) ** 2
+
+    return 10 * math.log10(max_square_error_sum / square_error_sum)
+
+
+def to_grayscale(image_state: ImageState, method: str) -> ImageState:
+    image = _to_grayscale(image_state.real_image, method=method)
+    return ImageState(visible_image=image, real_image=image)
+
+
+TO_GRAYSCALE_METHODS = ['mean', 'CCIR 601-1']
+
+
+def _to_grayscale(image: PIL.Image.Image, method: str) -> PIL.Image.Image:
+    if image.mode == 'L':
+        return image
+
+    if image.mode != 'RGB':
+        raise ValueError(
+            'Cannot convert image in mode {image.mode} to grayscale. '
+            'Image mode must be RGB.'
+        )
+
+    if method not in TO_GRAYSCALE_METHODS:
+        raise ValueError(
+            f'Unknown method {method} for conversion to grayscale.'
+    )
+
+    if method == 'mean':
+        return _to_grayscale_mean(image)
+    return _to_grayscale_ccir601(image)
+
+
+def _to_grayscale_mean(image: PIL.Image.Image) -> PIL.Image.Image:
+    array = np.asarray(image)
+    grayscale_array = (array.sum(axis=2) / 3).astype(np.uint8)
+    grayscale_image = PIL.Image.fromarray(grayscale_array, mode='L')
+    grayscale_image.filename = image.filename
+    return grayscale_image
+
+
+def _to_grayscale_ccir601(image: PIL.Image.Image) -> PIL.Image.Image:
+    array = np.asarray(image).astype(int)
+    grayscale_array = ((77 * array[:,:,0] + 150 * array[:,:,1] + 29 * array[:,:,2]) / 256).astype(np.uint8)
+    grayscale_image = PIL.Image.fromarray(grayscale_array, mode='L')
+    grayscale_image.filename = image.filename
+    return grayscale_image
+
+
--- image_cnc/gui.py
+++ image_cnc/gui.py
@@ -2,7 +2,6 @@ import io
 import os.path
 
 import PySimpleGUI as sg
-import PIL.ImageTk
 
 import core
 
@@ -30,10 +29,22 @@ def main():
         if event == sg.WIN_CLOSED:
             break
 
+        # chose working image
+        if values['working_image_dropdown'] == 'left':
+            working_image = image1
+            working_image_element_key = 'image1_element'
+        else:
+            working_image = image2
+            working_image_element_key = 'image2_element'
+
         # image1 events
         if event == 'upload_image1_filepath':
             image1 = core.upload_image(values['upload_image1_filepath'])
             window['image1_element'].update(data=core.image_to_bytes(image1), size=(512, 512))
+
+            # update working image
+            if values['working_image_dropdown'] == 'left':
+                working_image = image1
         elif event == 'save_as_image1_dummy_input':
             filepath = values['save_as_image1_dummy_input']
             core.save_image(filepath, image1)
@@ -42,18 +53,53 @@ def main():
         elif event == 'upload_image2_filepath':
             image2 = core.upload_image(values['upload_image2_filepath'])
             window['image2_element'].update(data=core.image_to_bytes(image2), size=(512, 512))
+
+            # update working image
+            if values['working_image_dropdown'] == 'right':
+                working_image = image2
         elif event == 'save_as_image2_dummy_input':
             filepath = values['save_as_image2_dummy_input']
             core.save_image(filepath, image2)
-        
+
+        # comparison events
+        elif event == 'calculate_psnr_button':
+            try:
+                psnr = core.psnr(image1, image2)
+            except ValueError as e:
+                sg.popup(e.args[0])
+                continue
+
+            if psnr == float('inf'):
+                result = '∞'
+            else:
+                result = f'{psnr:.4f}'
+            window['calculate_psnr_result'].update(result)
+
+        # instruments events
+        elif event == 'convert_to_grayscale_button':
+            method = values['convert_to_grayscale_method_dropdown']
+            working_image = core.to_grayscale(working_image, method=method)
+
+            window[working_image_element_key].update(
+                data=core.image_to_bytes(working_image), size=(512, 512)
+            )
+
+        # if working image has changed, update left or right image accordingly
+        if values['working_image_dropdown'] == 'left':
+            image1 = working_image
+        else:
+            image2 = working_image
 
 
 def setup_layout(default_image1, default_image2):
     image1_frame = setup_image_frame('image1', default_image1)
     image2_frame = setup_image_frame('image2', default_image2)
+    instruments_frame = setup_instruments_frame()
+    comparison_frame = setup_comparison_frame()
 
     return [
-        [image1_frame, image2_frame]
+        [image1_frame, image2_frame, instruments_frame],
+        [comparison_frame]
     ]
 
 
@@ -67,7 +113,8 @@ def setup_image_frame(image_name, default_image):
         'Upload image', target=f'upload_{image_name}_filepath'
     )
     upload_image_filepath = sg.Input(
-        default_image.filename, key=f'upload_{image_name}_filepath',
+        core.image_filename(default_image),
+        key=f'upload_{image_name}_filepath',
         disabled=True, enable_events=True,
     )
 
@@ -86,6 +133,47 @@ def setup_image_frame(image_name, default_image):
     ]
     image_frame = sg.Frame('', image_frame_layout)
     return image_frame
+
+
+def setup_instruments_frame():
+    working_image_text = sg.Text('Image: ')
+    working_image_dropdown = sg.Combo(
+        ['left', 'right'], key='working_image_dropdown',
+        default_value='left', readonly=True, enable_events=True
+    )
+
+    convert_to_grayscale_button = sg.Button(
+        'Convert to grayscale', key='convert_to_grayscale_button'
+    )
+    convert_to_grayscale_text = sg.Text('Method: ')
+    convert_to_grayscale_method_dropdown = sg.Combo(
+        core.TO_GRAYSCALE_METHODS, key='convert_to_grayscale_method_dropdown',
+        default_value=core.TO_GRAYSCALE_METHODS[0], readonly=True,
+    )
+
+    instruments_frame_layout = [
+        [working_image_text, working_image_dropdown],
+        [convert_to_grayscale_button],
+        [convert_to_grayscale_text, convert_to_grayscale_method_dropdown],
+    ]
+    instruments_frame = sg.Frame('', instruments_frame_layout)
+
+    return instruments_frame
+
+
+def setup_comparison_frame():
+    calculate_psnr_button = sg.Button(
+        'Calculate PSNR', key='calculate_psnr_button'
+    )
+    calculate_psnr_result = sg.Text('', key='calculate_psnr_result', size=(10, 1))
+    comparison_frame_layout = [
+        [calculate_psnr_button],
+        [calculate_psnr_result],
+    ]
+
+    comparison_frame = sg.Frame('', comparison_frame_layout)
+
+    return comparison_frame
 
 
 if __name__ == '__main__':
